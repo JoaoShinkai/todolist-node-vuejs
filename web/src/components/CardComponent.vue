85bc46038744b75f54faccc847de88cf5c0a43a3
unknown <joaogabriel1412@gmail.com>
2022-07-13 12:10:31 -0300

    Create card component

---
+++ web/src/components/CardComponent.vue
@@ -0,0 +1,77 @@
+<template>
+    <v-card
+    :loading="loading"
+    class="todolist-card"
+    width="374"
+    color="#2f2e36"
+  >
+    <template slot="progress">
+      <v-progress-linear
+        color="deep-purple"
+        height="10"
+        indeterminate
+      ></v-progress-linear>
+    </template>
+
+    <v-card-title>Lembrete agendado para: </v-card-title>
+
+    <v-card-text style="flex: 1">
+      
+        <div class="todolist-card-dategroup">
+            <div>14/12/2001</div>
+            <div>14:30</div>
+        </div>
+
+        <div class="my-4 todolist-card-subtitle">
+            Detalhes
+        </div>
+
+        <div>{{description}}</div>
+    </v-card-text>
+
+    <v-divider class="mx-4"></v-divider>
+
+    <v-card-actions>
+      <v-btn
+        color="pink lighten-1"
+        text
+        @click="reserve"
+      >
+        Excluir
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+<script>
+export default {
+    data() {
+        return{
+            loading: false,
+        }
+    },
+    props: {
+        description: String,
+        date: Date
+    }
+}
+</script>
+
+<style>
+.todolist-card{
+    display: flex;
+    flex-direction: column;
+}
+.todolist-card-dategroup{
+    display: flex;
+    grid-gap: 8px;
+}
+.todolist-card-dategroup div{
+    background-color: #9575cd;
+    padding: 6px 10px;
+    border-radius: 18px;
+}
+.todolist-card-subtitle{
+    font-size: 1.25rem;
+    font-weight: 500;
+}
+</style>
--- web/src/views/DashboardView.vue
+++ web/src/views/DashboardView.vue
@@ -1,17 +1,53 @@
 <template>
     <div class="todolist-dashboard">
-        Loggedadsdasd 
+        <div class="todolist-dashboard-button">
+            <v-btn class="mx-2" fab dark color="deep-purple lighten-2">
+                <v-icon dark>
+                    mdi-plus
+                </v-icon>
+            </v-btn>
+        </div>
+        <div class="todolist-dashboard-schedules">
+            <card-component  v-for="schedule in this.schedules" :key="schedule.id" :description="schedule.description"/>
+        </div>
     </div>
 </template>
 
 <script>
+// import jwt_decode from 'jwt-decode'
+import axios from 'axios'
+import CardComponent from '../components/CardComponent.vue'
 
 export default {
     name: 'DashboardView',
     data(){
         return{
-
+            token: localStorage.getItem('token'),
+            schedules: []
         }
+    },
+    components: {
+        CardComponent
+    },
+    methods: {
+        async loadSchedules(){
+            try{
+                var req = {
+                    headers: {
+                        Authorization: `Bearer ${this.token}`
+                    }
+                }
+
+                const result = await axios.get('http://localhost:3300/scheduling/', req);
+
+                this.schedules = result.data
+            }catch(err){
+                console.log(err);
+            }
+        }
+    },
+    created: async function() {
+        await this.loadSchedules()
     }
 }
 </script>
@@ -20,5 +56,17 @@ export default {
 .todolist-dashboard{
     background-color: #23222a;
     min-height: 100vh;
+    padding: 10px;
+}
+.todolist-dashboard-schedules{
+    display: grid;
+    grid-template-columns: repeat(auto-fill, 364px);
+    grid-gap: 10px;
+    justify-content: center;
+}
+.todolist-dashboard-button{
+    display: flex;
+    justify-content: end;
+    padding: 15px 0;
 }
 </style>
